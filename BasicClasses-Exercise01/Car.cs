91afe172c0eb958c71983f8755ffe19157d32c54
lostinthestack <lostinthestack@protonmail.com>
2021-10-07 14:49:17 -0500

    Init

---
+++ BasicClasses-Exercise01/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicClasses_Exercise01
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ BasicClasses-Exercise01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicClasses_Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car()
+            {
+                Make = "Honda",
+                Model = "Civic",
+                Year = 2011
+            };
+
+            Console.WriteLine($"Vehicle make:    {myCar.Make}");
+            Console.WriteLine($"Vehicle model:    {myCar.Model}");
+            Console.WriteLine($"Vehicle year:    {myCar.Year}");
+        }
+    }
+}
